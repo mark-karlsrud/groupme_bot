072f28c2d796228ebd849ddc00b1834c2a3ea8fb
Mark Karlsrud <markkarlsrud@gmail.com>
2020-07-27 22:07:11 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# groupme_bot
-Groupme Bot
+# Groupme Bot
+
+A bot to reply with an image or image link related to your search terms.
+
+## How to use
+In a GroupMe rooms, type `@computer <search terms>`
+
+## Setup
+1. Create a GroupMe application here: https://dev.groupme.com/
+2. In `groupme.properties`, set the `access_token`
+3. Create a bot for each room here: https://dev.groupme.com/bots
+4. In `groupme.properties`, add the Bot ID to the rooms list like this:
+```
+room_name=bod_id
+```
+5. Figure out your public IP address of the device you will be running this code on
+https://www.whatismyip.com/what-is-my-public-ip-address/
+6. Open the port
+https://www.noip.com/support/knowledgebase/general-port-forwarding-guide/
+7. Set the Callback URL for each bot to `http://<your_ip_address>/groupme/<room_name>`
+8. Register a Reddit app
+https://www.reddit.com/wiki/api#wiki_reddit_api_access
+9. Update `groupme.properties` with `reddit_id`, `reddit_secret`, `reddit_username`, `reddit_token`
+10. Configure as you please the following: `reddit_nsfw`, `subreddits`, `backup_subreddit`, `bot_name`
+
+## How to run
+`python3 groupme.py`
+You may need to install the appropriate python packages.
---
+++ groupme.py
@@ -0,0 +1,153 @@
+#!/usr/bin/python    
+import configparser
+import os
+import json
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from flask import Flask, request
+import requests
+import random
+import praw
+
+config = configparser.RawConfigParser()
+config.read('groupme.properties')
+
+subreddits = config.get('Reddit', 'subreddits')
+backup_subreddit = config.get('Reddit', 'backup_subreddit')
+
+access_token = config.get('GroupMe', 'access_token')
+bot_name = config.get('GroupMe', 'bot_name')
+
+reddit_id = config.get('Reddit', 'reddit_id')
+reddit_secret = config.get('Reddit', 'reddit_secret')
+reddit_username = config.get('Reddit', 'reddit_username')
+reddit_token = config.get('Reddit', 'reddit_token')
+reddit_useragent = 'rpi:com.' + reddit_username + '.groupme:v1 (by /u/' + reddit_username + ')'
+reddit_nsfw = config.get('Reddit', 'reddit_nsfw')
+
+reddit = praw.Reddit(client_id=reddit_id, client_secret=reddit_secret, user_agent=reddit_useragent)
+reddit.read_only = True
+
+app = Flask(__name__)
+
+# Called every time a message is sent in the group
+@app.route('/groupme/<channel>', methods=['POST'])
+def webhook(channel):
+        # 'message' is an object that represents a single GroupMe message.
+        message = request.get_json()
+        if sender_is_bot(message):
+                return "ok", 200
+        bot_id = config.get('GroupMeRooms', channel)
+
+        text = message['text']
+        if text.startswith('@' + bot_name):
+            search_terms = text.replace('@' + bot_name,'')
+            if not send_reddit_image(search_terms, bot_id):
+                if not send_reddit_image_subreddit(search_terms, bot_id, backup_subreddit):
+                    reply("Can't help you there...", bot_id)
+        
+        print('returning...')
+        return "ok", 200
+
+################################################################################
+
+def send_error(message):
+    bod_id = config.get('GroupMeRooms', 'test')
+    reply('error: ' + str(message), bod_id)
+
+# Send a message in the groupchat
+def reply(msg, bot_id):
+        url = 'https://api.groupme.com/v3/bots/post'
+        data = {
+                'bot_id'                : bot_id,
+                'text'                  : msg
+        }
+        request = Request(url, urlencode(data).encode())
+        json = urlopen(request).read().decode()
+
+# Send a message with an image attached in the groupchat
+def reply_with_image(msg, imgURL, bot_id):
+        url = 'https://api.groupme.com/v3/bots/post'
+        urlOnGroupMeService = upload_image_to_groupme(imgURL)
+        data = {
+                'bot_id'                : bot_id,
+                'text'                  : msg,
+                'picture_url'           : urlOnGroupMeService
+        }
+        request = Request(url, urlencode(data).encode())
+        json = urlopen(request).read().decode()
+        return urlOnGroupMeService
+        
+# Uploads image to GroupMe's services and returns the new URL
+def upload_image_to_groupme(imgURL):
+        imgRequest = requests.get(imgURL, stream=True)
+        for response in imgRequest.history:
+            if response.status_code == 302:
+                return
+        filename = 'temp.png'
+        postImage = None
+        print(imgURL)
+        if imgRequest.status_code == 200:
+                # Save Image
+                with open(filename, 'wb') as image:
+                        for chunk in imgRequest:
+                                image.write(chunk)
+                # Send Image
+                headers = {'content-type': 'application/json'}
+                url = 'https://image.groupme.com/pictures'
+                files = {'file': open(filename, 'rb')}
+                payload = {'access_token': access_token}
+                r = requests.post(url, files=files, params=payload)
+                imageurl = r.json()['payload']['url']
+                os.remove(filename)
+                return imageurl
+
+# Checks whether the message sender is a bot
+def sender_is_bot(message):
+        return message['sender_type'] == "bot"
+
+################################################################################
+
+def send_reddit_image(search_terms, bot_id):
+    return send_reddit_image_subreddit(search_terms, bot_id, subreddits.replace(',','+'))
+
+def send_reddit_image_subreddit(search_terms, bot_id, subreddits):
+        results = list(reddit.subreddit(subreddits).search(search_terms, params={'include_over_18': reddit_nsfw}))
+        random.shuffle(results)
+        for result in results:
+            image_url = result.url
+            try:
+                # print(str(vars(result)))
+                if image_url:
+                    if 'gfycat' in image_url:
+                        try:
+                            # get redirected url
+                            response = requests.get(result.url)
+                            reply(response.url, bot_id)
+                        except Exception as e:
+                            reply(result.url, bot_id)
+                        return True
+                    else:
+                        if reply_with_image('', image_url, bot_id):
+                            return True
+                else:
+                    #TODO fallback to other site?
+                    send_error('no image url')
+            except Exception as e:
+                print(e)
+        return False
+
+def get_gfycat_url(url):
+        base_url = 'https://api.gfycat.com/v1/gfycats/'
+        id = url.split('/')[-1]
+        response = requests.get(base_url + id)
+        if response.status_code == 200:
+            return response.json()['gfyItem']['gifUrl']
+        else:
+            send_error(response.status_code)
+            return None
+
+################################################################################
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=config.get('Server', 'port'))
